25fd9332af513ee98ef45069b4b6f086dc147b44
Varun Thaker <vthakar3@gmail.com>
2023-03-29 15:57:04 +0200

    first version

---
+++ ecom/src/pages/checkout/Checkout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './checkout.css'
+
+const Checkout = () => {
+  return (
+    <div className='item-checkout'>Page under Progress</div>
+  )
+}
+
+export default Checkout
